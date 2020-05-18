26d7b1584249d8d04bf0550ef94b6837e4797d8d
Leo Carvalho <carvalho.csleo@gmail.com>
2020-05-18 16:13:18 -0300

    Alter rule to users SQL

--- src/database/migrations/20200517211518_create_users.js
+++ src/database/migrations/20200517211518_create_users.js
@@ -2,8 +2,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', function(table) {
     table.increments('id').primary();
-    table.string('email').notNullable();
-    table.string('username', 50).notNullable();
+    table.string('email').notNullable().unique();
+    table.string('username', 50).notNullable().unique();
     table.string('password').notNullable();
     table.boolean('is_active').notNullable();
     table.integer('permission_id').defaultTo(1);
