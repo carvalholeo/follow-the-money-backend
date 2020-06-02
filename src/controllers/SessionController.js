8fdc3c0b133e21d44240689847599b2ce76c90e8
Leo Carvalho <carvalho.csleo@gmail.com>
2020-06-02 00:39:43 -0300

    Refactor to a centralized user_id validation

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,12 +1,15 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const connection = require('../database/connection');
 const generateUniqueId = require('../utils/generateUniqueId');
+const getUserId = require('../utils/getUserId');
+const { expires_in, secret } = require('../config/auth');
 
 module.exports ={
     async create(request, response) {
         try {
-            const { username, password } = request.body;
+            const { username, password, remember = false } = request.body;
             const { ip_address } = request.connection.remoteAddress;
             const { user_agent } = request.headers["user-agent"];
             const authorization_id = generateUniqueId();
@@ -15,7 +18,7 @@ module.exports ={
                 .where({
                     username
                 })
-                .select('id', 'password', 'is_active')
+                .select('id', 'username', 'password', 'is_active')
                 .first();
             
             const db_password = user.password;
@@ -25,6 +28,10 @@ module.exports ={
                 return response.status(401)
                     .json({ error: "User or password is incorrect. Try again." });
             }
+
+            user.password = undefined;
+
+            const time_to_expire = !remember ? expires_in : '';
 
             if(!user.is_active) {
                 return response.status(403)
@@ -38,8 +45,12 @@ module.exports ={
                     user_agent
                 });
 
+            const token = jwt.sign({ user: user.username }, secret, {
+                expiresIn: time_to_expire,
+            });
+
             return response.status(200)
-                .json({ token: authorization_id });
+                .json({ user: user.username, token, session: authorization_id });
         } catch (error) {
             return response.status(500)
                 .json({ error: "There was an error in server. Please, try again later. For support, contact to the system administrator." });
@@ -49,7 +60,7 @@ module.exports ={
 
     async destroy(request, response) {
         try {
-            const authorization_id = request.headers.token;
+            const authorization_id = request.headers.session;
 
             const authorization_deleted = await connection('sessions')
                 .where('authorization_id', authorization_id)
@@ -59,6 +70,24 @@ module.exports ={
                 return response.status(417)
                     .json({ error: "Token passed is invalid. Try again with a valid token." });
             }
+
+            return response.status(200)
+                .json({ message: "Loggout successfully." }); 
+        } catch (error) {
+            return response.status(500)
+                .json({ message: "There was an internal error. Probably, you're now logout, but we can't ensure that. If you need to be sure it, please clean your browser data, cookies, session and cache." })
+        }
+        
+    },
+
+    async destroyAll(request, response) {
+        try {
+            const authorization_id = request.headers.session;
+            const user_id = await getUserId(authorization_id);
+
+            const authorization_deleted = await connection('sessions')
+                .where('user_id', '=', user_id)
+                .del('*'); 
 
             return response.status(200)
                 .json({ message: "Loggout successfully." }); 
