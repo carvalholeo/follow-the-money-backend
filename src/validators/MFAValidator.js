81eaf322c97c3ecd7da66cde65de9a79dc910692
Leo Carvalho <carvalho.csleo@gmail.com>
2020-06-02 22:01:48 -0300

    Add MFA Validator to celebrate

---
+++ src/validators/MFAValidator.js
@@ -0,0 +1,13 @@
+const { Segments, Joi } = require('celebrate');
+const { headers } = require('./TokenValidator');
+
+module.exports = {
+    mfaRequired() {
+        return {
+            headers,
+            [Segments.BODY]: Joi.object().keys({
+                mfa_code: Joi.string().min(6).max(6).required()
+            })
+        };
+    }
+}
