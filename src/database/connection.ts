e586b137120537030609edca5afbcd1522cd1846
Leo Carvalho <lcarvalho@digitalhouse.com>
2020-06-26 21:56:19 -0300

    Update export default

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,7 +1,7 @@
 import knex from 'knex';
-import configuration from '../../knexfile';
+const configuration = require('../../knexfile');
 
-const config = process.env.NODE_ENV == 'test' ? configuration.test : configuration.development;
+const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
 
 const connection = knex(config);
 
