8c902b646f444dd462f4c66e3f574f6e383cb2ff
Leo Carvalho <carvalho.csleo@gmail.com>
2020-06-29 20:58:05 -0300

    Fix quotes style

--- knexfile.ts
+++ knexfile.ts
@@ -1,57 +1,57 @@
-import path from 'path';
+import path from "path";
 // Update with your config settings.
 
 module.exports = {
 
   development: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host : '127.0.0.1',
-      database: 'ftm',
-      user:     'root',
-      password: ''
+      host : "127.0.0.1",
+      database: "ftm",
+      user:     "root",
+      password: ""
     },
     migrations: {
-      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
-      extension: 'ts',
-      tableName: 'knex_migrations'
+      directory: path.resolve(__dirname, "src", "database", "migrations"),
+      extension: "ts",
+      tableName: "knex_migrations"
     },
     pool: {
       min: 1,
       max: 10
     },
     seeds: {
-      directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
-      extension: 'ts'
+      directory: path.resolve(__dirname, "src", "database", "seeds"),
+      extension: "ts"
     },
     useNullAsDefault: true,
     acquireConnectionTimeout: 10000,
   },
 
   test: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './src/database/test.sqlite'
+      filename: "./src/database/test.sqlite"
     },
     migrations: {
-      directory: './src/database/migrations',
-      tableName: 'knex_migrations'
+      directory: "./src/database/migrations",
+      tableName: "knex_migrations"
     },
     useNullAsDefault: true,
   },
 
   staging: {
-    client: 'mysql',
+    client: "mysql",
     version: 10,
     connection: {
-      host : '127.0.0.1',
-      database: 'ftm',
-      user:     'root',
-      password: ''
+      host : "127.0.0.1",
+      database: "ftm",
+      user:     "root",
+      password: ""
     },
     migrations: {
-      directory: './src/database/migrations',
-      tableName: 'knex_migrations'
+      directory: "./src/database/migrations",
+      tableName: "knex_migrations"
     },
     pool: {
       min: 1,
@@ -61,12 +61,12 @@ module.exports = {
   },
 
   production: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host : '127.0.0.1',
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host : "127.0.0.1",
+      database: "my_db",
+      user:     "username",
+      password: "password"
     },
     pool: {
       min: 1,
@@ -74,8 +74,8 @@ module.exports = {
     },
     acquireConnectionTimeout: 10000,
     migrations: {
-      directory: './src/database/migrations',
-      tableName: 'knex_migrations'
+      directory: "./src/database/migrations",
+      tableName: "knex_migrations"
     }
   }
 
