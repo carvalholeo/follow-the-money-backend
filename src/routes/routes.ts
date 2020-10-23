0c4822241f1ee673180a7f34e4be055a336a2b35
Leonardo Carvalho <carvalho.csleo@gmail.com>
2020-10-22 23:55:11 -0300

    Refactoring Routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,9 @@
 import express from "express";
 
-import v1 from "./v1";
+import version_1 from "./version_1";
 
 const routes = express.Router();
 
-routes.use("/v1", v1);
+routes.use("/1", version_1);
 
 export default routes;
