02d9549430dac1a0b4af49db79a510208fa2f2d9
Leonardo Carvalho <carvalho.csleo@gmail.com>
2020-10-25 21:33:31 -0300

    Rename file

--- src/config/options.ts
+++ src/config/options.ts
@@ -1,9 +1,9 @@
 const expire = Math.floor(Date.now() / 1000) - 30;
-const secret = "W@#!90v9V22^SGy#jWe86$aUM%J02bmF";
+const secret = 'W@#!90v9V22^SGy#jWe86$aUM%J02bmF';
 
 const options = {
   expires_in: expire,
   secret
-}
+};
 
 export default options;
